76072a3e454b5d2ac7da286e5a4e28990500fe2f
huangxin <1761661593@qq.com>
2020-12-26 12:12:58 +0800

    建立每个类

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String ags[]){
+        sacanner("请输入账号：");
+        System.out.println("");
+
+    }
+}
---
+++ src/user.java
@@ -0,0 +1,39 @@
+class user{
+
+       private String usersname;
+       private String password;
+       private String address;
+       private String phone;
+
+    public String getUsersname() {
+        return usersname;
+    }
+
+    public void setUsersname(String usersname) {
+        this.usersname = usersname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
